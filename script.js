6572af9ac65edb4382fbc94812cd39d30b2d1082
Arad <aradkuhzad@gmail.com>
2026-02-20 13:51:44 +0330

    Enhance OPors function with additional comparison operations and implement checkAgeCategory function for age classification, improving script.js functionality and user interaction.

--- script.js
+++ script.js
@@ -85,9 +85,13 @@ function OPors() {
   console.log(++x);
   console.log(--y);
   console.log(x ** y);
+  console.log(x % y);
   console.log(x *= 5);
   console.log(y -= 1);
   console.log(x += 3);
+  console.log(x == y);
+  console.log(x);
+  console.log(y);
 }
 OPors();
 }
