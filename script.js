9c0704dd88737536b6e68fcebc19ad4e06d635b1
Arad <aradkuhzad@gmail.com>
2026-02-20 22:41:49 +0330

    Implement lamp image toggle functionality and brightness adjustment in script.js; update styles.css to enhance lamp interaction with cursor pointer.

--- script.js
+++ script.js
@@ -4,12 +4,6 @@ function ShowDate() {
 function Show() {
   document.getElementById("showmessage").innerHTML = "Hello World";
 }
-function activate() {
-  document.getElementById("picture-lamp").src = "img/pic_bulbon.gif";
-}
-function deactivate() {
-  document.getElementById("picture-lamp").src = "img/pic_bulboff.gif";
-}
 function changefont() {
   document.getElementById("change-fontsize").style.fontSize = "40px";
 }
