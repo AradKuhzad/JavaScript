5ecbd1e32199867c6c6d3e75bad1d16e17b7cf87
Arad <aradkuhzad@gmail.com>
2026-07-03 18:57:22 +0330

    Add more number properties to testNumbers function in script.js, including MAX_VALUE, MIN_VALUE, and positive infinity for enhanced testing of number behaviors.

--- script.js
+++ script.js
@@ -411,6 +411,9 @@ testStrings();
 function testNumbers() {
   console.log(Number.MAX_SAFE_INTEGER);
   console.log(Number.MIN_SAFE_INTEGER);
+  console.log(Number.MAX_VALUE);
+  console.log(Number.MIN_VALUE);
+  console.log(Number.POSITIVE_INFINITY);
   console.log(Number.NEGATIVE_INFINITY);  
   console.log(Number.NaN);
   console.log(Number.EPSILON);
