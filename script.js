2a5641612ce056cfba79674d56adc8dc57165904
Arad <aradkuhzad@gmail.com>
2026-07-09 21:47:02 +0330

    Add further tests to testNumbers function in script.js, including checks for non-integer values, finiteness, NaN detection, and string parsing to numbers for comprehensive validation of number behaviors.

--- script.js
+++ script.js
@@ -419,6 +419,7 @@ function testNumbers() {
   console.log(Number.EPSILON);
   console.log(Math.abs(0.1 + 0.2 - 0.3) < Number.EPSILON); //true
   console.log(Number.isInteger(5)); //true
+  console.log(Number.isInteger(5.5)); //false
 }
 testNumbers();
 
