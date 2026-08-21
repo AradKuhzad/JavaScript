643c3a76972704c69885365e4bb910a59e9ef5ef
Arad <aradkuhzad@gmail.com>
2026-08-21 19:36:08 +0330

    Add new regex matching for colors in Iterationstest function to enhance demonstration

--- script.js
+++ script.js
@@ -677,5 +677,6 @@ function Iterationstest() {
     let Changec = companies.replace(/Microsoft/i, "Apple");
     console.log(Changec);
     let colors = "red, white, black, yellow, green, purple, gold, grey";
+    let result = colors.match(/green|purple|gold/g);
   }
   RegExpp();
