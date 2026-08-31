454441c01ac97110fdbd710cb8537e3ace300eb7
Arad <aradkuhzad@gmail.com>
2026-08-31 23:01:58 +0330

    Update

--- script.js
+++ script.js
@@ -688,6 +688,7 @@ function Iterationstest() {
     console.log(isNaN(x));
   }
   datatypes();
+    let x = document.getElementById("inputbox").value;
     try { 
       if(x.trim() == "")  throw "empty";
       if(isNaN(x)) throw "not a number";
