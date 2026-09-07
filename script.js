3594134d554f42a1cf8aa37f7e2261a1db90ea90
Arad <aradkuhzad@gmail.com>
2026-09-07 21:16:30 +0330

    Update

--- script.js
+++ script.js
@@ -789,6 +789,8 @@ function Iterationstest() {
     console.log("navigator.cookieEnabled is " + navigator.cookieEnabled);
     console.log("Navigator.online is " + navigator.onLine);
     console.log("Navigator.appCodeName is " + navigator.appCodeName);
+    let text;
+    if (confirm("Press a button!")) {
       text = "You pressed OK!";
     } else {
       text = "You pressed Cancel!"
