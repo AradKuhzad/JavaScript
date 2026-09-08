e8c59c8d59236e71d1b4488323cc1c0a3f6a8a89
Arad <aradkuhzad@gmail.com>
2026-09-08 16:01:41 +0330

    Update

--- JS-SimpleProjects/FetchAPI2.js
+++ JS-SimpleProjects/FetchAPI2.js
@@ -1,4 +1,6 @@
 fetch("https://jsonplaceholder.typicode.com/users/3")
+    myDisplayer(data.name);
+})
 function myDisplayer(text) {
     document.getElementById("FetchAPItest").innerHTML = text;
 }
