ee81debbaf78411def109e0366bd79f4b75c0c67
Arad <aradkuhzad@gmail.com>
2026-09-10 09:13:53 +0330

    Update

--- JS-SimpleProjects/FetchAPI2.js
+++
@@ -1,9 +0,0 @@
-fetch("https://jsonplaceholder.typicode.com/users/3")
-.then( response => response.json())
-.then(data => {
-    console.log(data);
-    myDisplayer(data.name);
-})
-function myDisplayer(text) {
-    document.getElementById("FetchAPItest").innerHTML = text;
-}
