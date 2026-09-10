a1996009fafb16ee75b574a6a2792fbdc309660b
Arad <aradkuhzad@gmail.com>
2026-09-10 09:13:57 +0330

    Update

--- JS-SimpleProjects/Loading-page.js
+++
@@ -1,9 +0,0 @@
-function EventTest3() {
-    window.addEventListener("load", function () {
-      setTimeout( function () {
-        document.getElementsByClassName("loadingcontent")[0].style.display = "none";
-        document.getElementsByClassName("allcontent")[0].style.display = "block";
-      }, 2000)
-    });
-  }
-  EventTest3();
