c403594fe736793cd4d7fb072cc9741582984f20
Arad <aradkuhzad@gmail.com>
2026-09-10 09:14:08 +0330

    Update

--- JS-SimpleProjects/NameKey.js
+++
@@ -1,7 +0,0 @@
-function EventTest2() {
-    const k = document.getElementById("k");
-    k.addEventListener("keydown", function (event) {
-      document.getElementById("namekey").innerHTML = "You pressed: " + event.key;
-    });
-  }
-  EventTest2(); 
