1832f1cf1a48b701cc54695565e12a268fa7794c
Arad <aradkuhzad@gmail.com>
2026-09-12 15:53:20 +0330

    Update

---
+++ Backend/NodeJS/sample.js
@@ -0,0 +1,20 @@
+const fs = require("fs").promises;
+
+async function getUserdata(userID) {
+    try {
+        const data = await fs.readFile("../../Frontend/data.json");
+        const users = JSON.parse(data);
+        const UserData = users.find(item => item.user.id === userID);
+
+        return UserData;
+    } catch (error) {
+        console.error("Failed to fetch user data:", error);
+        throw error;
+    }
+}
+
+async function main() {
+    const data = await getUserdata(102);
+    console.log(data);    
+}
+main();
