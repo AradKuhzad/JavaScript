bbb0d12e94d63bff7df38859eb7021f8c2dc83be
Arad <aradkuhzad@gmail.com>
2026-09-13 14:36:46 +0330

    Add whitespace for better code organization before HTTP server setup

--- Backend/NodeJS/sample.js
+++ Backend/NodeJS/sample.js
@@ -35,6 +35,9 @@ myPromis
     .then(result => console.log('Success: ', result))
     .catch(error => console.error('Error: ', error.message));
 
+//
+
+const http = require('http');
 http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end("Hello there!");
