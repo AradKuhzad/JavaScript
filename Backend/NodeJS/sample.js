58d3c0a1aa9788a0403467a729e8b328e71333b4
Arad <aradkuhzad@gmail.com>
2026-09-13 09:08:28 +0330

    Fix promise resolution in myPromis to ensure success message is logged

--- Backend/NodeJS/sample.js
+++ Backend/NodeJS/sample.js
@@ -18,6 +18,8 @@ async function main() {
     console.log(data);    
 }
 main();
+            resolve('Operation completed successfully');
+        } else {
             reject(new Error('Operation faild'));
         }
     }, 1000);
